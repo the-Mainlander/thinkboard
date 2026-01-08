fc1a3c6b5b4d932e42118790121e1761d7c76f09
dvr <thedhruvaraja@gmail.com>
2026-01-08 19:26:12 +0530

    added env variables, mongoDB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,21 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
 
-const app = express()
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
+const app = express();
+const PORT = process.env.PORT || 5001
+
+connectDB();
 
 app.use("/api/notes",notesRoutes); 
 
 app.listen(5001, () => {
-    console.log("Server started on PORT: 5001");
+    console.log("Server started on PORT: ",PORT);
 });
+
+// pass : euQS1pgQGoFSlZ84
